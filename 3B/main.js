f0bddfcf05ddc025e14d30e71726dc419ca8dc80
Pedro-H-Ferrer <105872503+Pedro-H-Ferrer@users.noreply.github.com>
2024-05-29 09:26:07 -0300

    Add files via upload

---
+++ 3B/main.js
@@ -0,0 +1,43 @@
+var cxPrincipal = document.querySelector("caixa-principal");
+var cxPerguntas = document.querySelector("caixa-perguntas");
+var cxAlternativas = document.querySelector("cx-alernativas");
+var cxRespostas = document.querySelector("cx-respostas");
+var txResultados = document.querySelector("tx-principal");
+
+var perguntas = [
+    {
+        enunciado = "A IA pode automatizar tarefas repetitivas.",
+        alternativas = [
+            "Vai eliminar tarefas monotonas",
+            "Vai tirar muitos empregos"
+        ]
+    },
+    {
+        enunciado = "A IA pode tomar decisões baseadas em dados.",
+        alternativas = [
+            "Maravilhoso",
+            "Assustador"
+        ]
+    },
+    {
+        enunciado = "A IA pode substituir certos empregos",
+        alternativas= [
+            "Não vejo problemas",
+            "É o fim dos empregos como conhecemos hoje"
+        ]
+    },
+    {
+        enunciado = "A IA pode ajudar a prever tendências de mercado",
+        alternativas = [
+            "Incrível",
+            "Sinistro"
+        ]
+    },
+    {
+        enunciado = "A IA pode trabalhar 24/7 sem pausas",
+        alternativas = [
+            "Surpreendente",
+            "Mão de obra barata"
+        ]
+    }
+]
